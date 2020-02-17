f32c127d30e16166e748e9f13d19dd6287fb2a39
Aparna Salokhe <asalokhe@carbonite.com>
2020-02-17 18:48:23 +0530

    Add andt textinput
    Modified the TextInput to use Antd input control & modified default props from Antd classes

--- ats-web/src/components/common/defaultProps.js
+++ ats-web/src/components/common/defaultProps.js
@@ -1,12 +1,15 @@
 export default class DefaultProps {
   constructor() { 
     this.errorMsg = '';
-    this.containerClass = "form-group";
-    this.containerErrorClass = "form-group has-error";
+    this.containerClass = "ant-form-item";
+    this.containerErrorClass = "ant-form-item has-error";
     this.controlClass = "col-sm-8";
-    this.labelClass = "control-label col-sm-4";
-    this.requiredLabelClass = "control-label required col-sm-4";
-    this.fieldClass = "form-control";
+    this.labelWrapperClass = "ant-col ant-form-item-label";
+    this.requiredLabelClass = "ant-form-item-required";
+    this.fieldContainerClass = "ant-col ant-form-item-control-wrapper";
+    this.fieldWrapperClass = "ant-form-item-control";
+    this.fieldClass = "ant-form-item-children";
+    this.inputControlClass = "ant-input";
     this.isRequired = false;
   };
 }
