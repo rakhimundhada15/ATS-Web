128182eac3bacd07b3144eb9f4bd6d3f2da442b5
rakhi.mundhada <rakhi.mundhada@alertlogic.com>
2020-02-20 12:08:36 +0530

    Rename startDate to date as review comments

--- ats-web/src/components/shared/datePicker.js
+++ ats-web/src/components/shared/datePicker.js
@@ -3,21 +3,21 @@ import PropTypes from "prop-types";
 import { DatePicker } from 'antd';
 
 function DateTimePicker(props) {
-    const [startDate, setStartDate] = useState(null);
-    const handleOnOk = (selectedDate) => {
-        if (selectedDate) {
-            props.onChange(selectedDate._d)
+    const [date, setDate] = useState(null);
+    const handleOnOk = (selectedDateObject) => {
+        if (selectedDateObject) {
+            props.onChange(selectedDateObject)
         }
     };
 
     return (
         <div className="ant-row ant-form-item">
-            <div className={!startDate ? props.containerErrorClass : props.containerClass}>
+            <div className={!date ? props.containerErrorClass : props.containerClass}>
                 <div className="ant-col ant-form-item-label ant-col-xs-24 ant-col-sm-8">
                     <label htmlFor={props.id} className={props.isRequired ? props.requiredLabelClass : ''}>{props.label}</label></div>
                 <div className={props.controlClass}>
-                    <DatePicker format="MM/DD/YYYY HH:mm" onChange={date => setStartDate(date)} showTime={true} disabledSeconds="true" onOk={date => handleOnOk(date)} />
-                    {!startDate && (
+                    <DatePicker format="MM/DD/YYYY HH:mm" onChange={date => setDate(date)} showTime={true} disabledSeconds="true" onOk={dateObj => handleOnOk(dateObj)} />
+                    {!date && (
                         <div className="ant-form-explain">{props.error}</div>
                     )}
                 </div>
