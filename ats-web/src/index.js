7e8dcf48ad6242cf056f85d60118941a021775e8
sharmagaurav1231 <41336259+sharmagaurav1231@users.noreply.github.com>
2020-02-20 15:30:36 +0530

    antD Dropdown (#13)
    * Dropdown
    
    * antD Dropdown
    
    * Dropdown Changes
    
    * Delete package-lock.json
    
    * Delete Dropdown.js

--- ats-web/src/index.js
+++ ats-web/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'antd/dist/antd.css';
+import './styles/shared.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
