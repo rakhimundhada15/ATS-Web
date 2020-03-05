7d8e98857fdf09205474f1e58fda65c84a91dccc
aparna-salokhe <aparna.salokhe@niyuj.com>
2020-03-05 16:12:00 +0530

    Added data table with search functionality (#18)
    * added dataTable
    
    * Added data table with search functionality
    
    * Updated model function name
    
    Co-authored-by: rakhimundhada <58169760+rakhimundhada@users.noreply.github.com>

---
+++ ats-web/src/components/shared/dataTable.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { Table, Button, Form, Input, Icon } from 'antd/lib';
+import PropTypes from "prop-types";
+
+class DataTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dataSource: this.props.dataSource
+        };
+    }
+    onChange = (pagination, filters, sorter, extra) => {
+        this.props.onChange(pagination, filters, sorter, extra);
+    }
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.form.validateFields((err, value) => {
+            if (!err && value.searchString) {
+                let filterArrayData = [];
+                this.props.dataSource.filter((obj) => {
+                    if (Object.values(obj).toString().toLocaleLowerCase().includes(value.searchString.toLocaleLowerCase())) {
+                        filterArrayData.push(obj);
+                    }
+                });
+                this.setState({
+                    dataSource: filterArrayData
+                });
+            }
+            else {
+                this.setState({
+                    dataSource: this.props.dataSource
+                });
+            }
+        });
+    };
+
+    showModal = () => {
+        this.props.showModal(true)
+
+    };
+    render() {
+        const { getFieldDecorator } = this.props.form;
+
+        return (
+            <div className='dataTable'>
+                <Form layout="inline" onSubmit={this.handleSubmit}>
+                    <Form.Item name="modelButton" hidden={!this.props.modelButtonLabel}>
+                        <Button
+                            htmlType="button"
+                            onClick={this.showModal}
+                        >
+                            {this.props.modelButtonLabel}
+                        </Button>
+                    </Form.Item>
+                    <Form.Item name="searchString">
+                        {getFieldDecorator("searchString")(
+                            <Input
+                                prefix={<Icon type="search" />}
+                                placeholder="search"
+                            />
+                        )}
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit">
+                            Go
+                    </Button>
+                    </Form.Item>
+                </Form>
+                <Table columns={this.props.columns} dataSource={this.state.dataSource} onChange={this.onChange} pagination={{ pageSize: 10 }} />
+            </div>
+        )
+    }
+
+}
+
+export default Form.create()(DataTable)
+
